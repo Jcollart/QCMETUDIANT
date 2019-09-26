15a30ed021a3fa01675006b69b27b34eba8586cf
teddycouaillier <teddy.couaillier@gmail.com>
2019-09-26 16:19:43 +0200

    Add background

--- index.php
+++ index.php
@@ -9,17 +9,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>QCM</title>
-    <link rel="stylesheet" href="public/css/index.css">
-    <link rel="stylesheet" href="public/css/admin.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
     crossorigin="anonymous">
+    <link rel="stylesheet" href="public/css/index.css">
+    <link rel="stylesheet" href="public/css/admin.css">
   </head>
   <body>
       <div class="py-5">
         <div class="row mx-0">
           <div class="col-lg-3 col-md-2 col-sm-1 col-xs-0 col-0"></div>
-          <div class="col-lg-6 col-md-8 col-sm-10 col-xs-12 col-0">
+          <div class="col-lg-5 col-md-8 col-sm-10 col-xs-12 col-0 mx-auto">
             <div class="bg-light shadow rounded p-4">
               <div class="text-center my-4">
                 <img src="./public/img/logoetudiant.png" width="150" height="150" class="rounded-circle shadow">
